a06657c4d80424cbd4a7b32e2f95516b67dcf5e4
bobyco <1417200023@qq.com>
2023-12-05 17:38:52 +0800

    s：增加默认市区

--- src/main/java/com/yanxuan88/australiacallcenter/common/Constant.java
+++ src/main/java/com/yanxuan88/australiacallcenter/common/Constant.java
@@ -3,10 +3,12 @@ package com.yanxuan88.australiacallcenter.common;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+import static java.time.ZoneId.SHORT_IDS;
+
 public interface Constant {
     String HEADER_TOKEN_KEY = "Authentication";
     String TOKEN_PAYLOAD_KEY = "uuid";
-    ZoneId DEFAULT_ZONE_ID = ZoneId.of("CTT");
+    ZoneId DEFAULT_ZONE_ID = ZoneId.of("CTT", SHORT_IDS);
     DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(DEFAULT_ZONE_ID);
     DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(DEFAULT_ZONE_ID);
 }
