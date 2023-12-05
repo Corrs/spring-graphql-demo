8deed17b41e5828c9b4079031ea59a28421d542d
bobyco <1417200023@qq.com>
2023-12-05 17:27:22 +0800

    s：增加常量

--- src/main/java/com/yanxuan88/australiacallcenter/common/Constant.java
+++ src/main/java/com/yanxuan88/australiacallcenter/common/Constant.java
@@ -1,6 +1,12 @@
 package com.yanxuan88.australiacallcenter.common;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public interface Constant {
     String HEADER_TOKEN_KEY = "Authentication";
     String TOKEN_PAYLOAD_KEY = "uuid";
+    ZoneId DEFAULT_ZONE_ID = ZoneId.of("CTT");
+    DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(DEFAULT_ZONE_ID);
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(DEFAULT_ZONE_ID);
 }
